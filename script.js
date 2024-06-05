1c5906956f8364222b8519516406db2f0b51bd86
Lucas Matheus <matheusalexandre012@gmail.com>
2024-06-04 22:23:22 -0300

    teste

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
 
-
+// Adicionar o vídeo do YouTube
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -14,12 +14,22 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function onYouTubeIframeAPIReady() {
     new YT.Player('player', {
-        videoId: 'p-EThRKoW5E', 
+        videoId: 'p-EThRKoW5E', // Substitua este ID pelo ID do vídeo desejado
         events: {
             'onReady': onPlayerReady
         }
     });
 }
+
+function playVideo() {
+    var player = document.getElementById('player');
+    var container = document.querySelector('.container');
+    var btnplay = document.querySelector('.btn-play');
+    container.style.display = "block" 
+    btnplay.style.display = "none" 
+    player.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+}
+
 
 function onPlayerReady(event) {
     event.target.playVideo();
